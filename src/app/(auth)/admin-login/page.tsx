2a1e8d55b397826273f933f50115f540eef535fa
asare-grant <p.asaregrant@gmail.com>
2026-01-18 11:35:37 +0000

    commit

--- src/app/(auth)/admin-login/page.tsx
+++ src/app/(auth)/admin-login/page.tsx
@@ -133,7 +133,7 @@ export default function AdminLoginPage() {
     <div className="min-h-screen flex items-center justify-center">
     {/* <div className="min-h-screen bg-slate-50 flex items-center justify-center"> */}
       <div className="w-[380px] rounded-2xl bg-white shadow-lg p-6">
-        <h1 className="text-xl font-semibold mb-1">Admin Dashboard</h1>
+        <h1 className="text-xl text-gray-700 font-semibold mb-1">Admin Dashboard</h1>
         <p className="text-sm text-gray-500 mb-6">
           Sign in to manage your platform
         </p>
